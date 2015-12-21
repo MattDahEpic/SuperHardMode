d8b0d15625517386a22d95b7b65fa90b2132fd75
Matt <mattdahepic@gmail.com>
2015-12-21 13:46:44 -0700

    partial work

--- src/main/java/com/mattdahepic/superhardmode/event/BreakEventHandler.java
+++ src/main/java/com/mattdahepic/superhardmode/event/BreakEventHandler.java
@@ -1,5 +1,10 @@
 package com.mattdahepic.superhardmode.event;
 
+import com.mattdahepic.superhardmode.config.SHMConfig;
+
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.BlockPos;
+import net.minecraft.util.EnumFacing;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.world.BlockEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
@@ -14,6 +19,15 @@ public class BreakEventHandler {
         handleSoftening(e);
     }
     private void handleSoftening (BlockEvent.BreakEvent e) {
-
+        if (SHMConfig.softenBlocksOnMine) {
+            ItemStack blockBroken = new ItemStack(e.world.getBlockState(e.pos).getBlock(),1,e.world.getBlockState(e.pos).getBlock().getMetaFromState(e.state));
+            if (SHMConfig.blocksThatSoften.contains(blockBroken)) {
+                for (EnumFacing side : EnumFacing.values()) {
+                    BlockPos pos = e.pos.offset(side);
+                    //TODO: check for blocks to break
+                    //TODO: break blocks
+                }
+            }
+        }
     }
 }
