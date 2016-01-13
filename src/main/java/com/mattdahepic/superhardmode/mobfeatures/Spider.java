3ba1b6aa451ac9b0eaae1b577b4ef5ff21899ec7
Matt <contact@mattdahepic.com>
2016-01-13 10:43:13 -0700

    add spider stuff part 2

--- src/main/java/com/mattdahepic/superhardmode/mobfeatures/Spider.java
+++ src/main/java/com/mattdahepic/superhardmode/mobfeatures/Spider.java
@@ -3,9 +3,15 @@ package com.mattdahepic.superhardmode.mobfeatures;
 import com.mattdahepic.mdecore.helpers.RandomHelper;
 import com.mattdahepic.superhardmode.config.SHMConfigMob;
 
+import net.minecraft.block.BlockAir;
+import net.minecraft.block.BlockCactus;
+import net.minecraft.block.BlockWeb;
 import net.minecraft.entity.monster.EntitySpider;
 import net.minecraft.entity.monster.EntityZombie;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.util.BlockPos;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.world.World;
 import net.minecraftforge.event.entity.EntityJoinWorldEvent;
 import net.minecraftforge.event.entity.living.LivingDeathEvent;
 import net.minecraftforge.event.entity.living.LivingSpawnEvent;
@@ -42,6 +48,26 @@ public class Spider {
         }
     }
     private static void webPlaceLogic (LivingDeathEvent e) {
-        //TODO: this
+        //TODO randomPoses
+        World world = e.entity.worldObj;
+
+        nextBlock:
+        for (BlockPos pos : randomPoses) {
+            if (!(world.getBlockState(pos).getBlock() instanceof BlockAir)) { //don't replace things that aren't air
+                continue;
+            }
+            for (EnumFacing side : EnumFacing.values()) {
+                if (world.getBlockState(pos.offset(side)).getBlock() instanceof BlockAir) { //don't place in midair
+                    continue nextBlock;
+                }
+                if (world.getBlockState(pos.offset(side)).getBlock() instanceof BlockCactus) { //don't place next to cactus (cause it'll break the cactus)
+                    continue nextBlock;
+                }
+            }
+            world.setBlockState(pos, BlockWeb.getStateById(0));
+            if (pos.getY() >= 63) { //above sea level //FIXME: world may have different sea level
+                //TODO clean up webs on surface after 30 seconds
+            }
+        }
     }
 }
